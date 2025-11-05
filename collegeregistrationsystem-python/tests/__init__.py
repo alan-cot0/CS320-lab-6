ea9c1182f91ba95cea97c3727af62eb9d423d274
ShinyTillDawn <160337699+alan-cot0@users.noreply.github.com>
2025-11-05 14:03:13 -0500

    Update __init__.py

---
+++ collegeregistrationsystem-python/tests/__init__.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.student import Student
+
+from tests.constants import STUDENT_FIRST_ID
+from tests.constants import STUDENT_1_FIRSTNAME_VALID
+from tests.constants import STUDENT_1_LASTNAME_VALID
+from tests.constants import STUDENT_1_DATEOFBIRTH_VALID
+from tests.constants import STUDENT_1_EMAILADDRESS_VALID
+from tests.constants import STUDENT_1_HOMEADDRESS_VALID
+from tests.constants import STUDENT_1_HOMEADDRESS_INVALID
